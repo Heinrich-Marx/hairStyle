ec878ba92f3bba610e6f7fc2e23e2c1c1227771b
dzmitry sotnikau <dzmitry.sotnikau@softnetix.io>
2023-01-10 09:17:07 +0300

    add scripts

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# hairStyle
+run build.sh to build packages
+
+run start.sh to up backend and frontend
+
+run finish.sh to stop backend and frontend
+
+8080 port for backend
+
+8020 port for frontend
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log(123)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          s <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
