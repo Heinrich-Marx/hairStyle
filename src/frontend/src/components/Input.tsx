22990acb843fdc6b65c7ee8dac1d9dba089b5192
dzmitry sotnikau <dzmitry.sotnikau@softnetix.io>
2023-01-19 22:16:57 +0300

    warning

--- src/frontend/src/components/Input.tsx
+++ src/frontend/src/components/Input.tsx
@@ -5,9 +5,8 @@ interface IInput {
     handle: Dispatch<SetStateAction<string>>
     className?: string
 }
-
 // @ts-ignore
-const Input = memo(({value, handle, className}) => {
+const Input = memo<IInput>(({value, handle, className}) => {
 	const onChange = (e:ChangeEvent<HTMLInputElement>) => handle(e.target.value);
 
 	return <input value={value} onChange={onChange} className={className}/>;
