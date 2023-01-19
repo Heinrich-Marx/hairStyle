867634e7725b7471866db1d51f4c5057c2c8e433
dzmitry sotnikau <dzmitry.sotnikau@softnetix.io>
2023-01-19 22:19:01 +0300

    fix error

--- src/frontend/src/components/Input.tsx
+++ src/frontend/src/components/Input.tsx
@@ -5,7 +5,7 @@ interface IInput {
     handle: Dispatch<SetStateAction<string>>
     className?: string
 }
-// @ts-ignore
+
 const Input = memo<IInput>(({value, handle, className}) => {
 	const onChange = (e:ChangeEvent<HTMLInputElement>) => handle(e.target.value);
 
